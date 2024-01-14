f156b765d4e505ade7d472b2fea4c181c74c98b1
Edisquall <152781383+Edisquall@users.noreply.github.com>
2024-01-14 15:51:04 +0000

    Student registration
    Working on the registration numb to be a valid registration

--- src/caprojectone/CaProjectone.java
+++ src/caprojectone/CaProjectone.java
@@ -20,10 +20,21 @@ public class CaProjectone {
         try {
             Scanner sc = new Scanner(new FileReader("C:\\Users\\Edi\\Documents\\NetBeansProjects\\caProjectone\\student.txt"));
             //Reading registrantion details from the file
+            
              while (sc.hasNextLine()) {
             String data = sc.nextLine();
+              //reading first line - Stundent name
+              
+            String classes = sc.nextLine();
+            //reading second line - Number of classes
+            
+            String registration = sc.nextLine();
+            //reading third line - Registration number
+            
+            int classesNum;
             String[] splitString = data.split(" ");
             //Splitting first name from the second name
+            
             if (splitString[0].matches("[a-zA-Z]+")) {
                 System.out.println("Valid name");
             } else {
@@ -35,12 +46,49 @@ public class CaProjectone {
             } else {
                 System.out.println("Second name must be only letters and numbers");
             } // Making sure second name has only letters and numbers
-            String data1 = sc.nextLine();
+            
+            //Working on a number os classes
+            
+            classesNum = Integer.parseInt(classes);
+            if (classesNum < 8 && classesNum >1) {
+                 System.out.println("Valid number os classes");
+             } else if (classesNum == 1 && classesNum == 8) {
+                 System.out.println("Valid number os classes");
+             }
+            else {
+                     System.out.println("Number of classes must be 1 to 8");
+                     } // Making sure that number of classes are valid
+            
+            // Working on registratio  number
+            
+            if (registration.length() < 6) {
+            System.out.println("Correct registration");
+        } else {
+                System.out.println("Incorrect registration");
+            } 
+            String first2 = registration.substring(0, 1);
+            // Separating the first 2 character to specify as a letter
+            
+            String first3 = registration.substring(2, 3);
+            // Separating the third and fourth character to specify as a number
+            
+            String first4 = registration.substring(4);
+            // Separating the first fifth character to specify as a letter or number
+            
+            if (first2.matches("[0-9]+")) {
+                System.out.println("First 2 characters are numbers");
+            } else if (first3.matches("[a-zA-Z]+")) {
+                System.out.println ("Third and fourth character must be letter");
+            } else if (first4.matches ("[0-9]+") || (first4.matches ("[a-zA-Z]+"))) {
+                System.out.println ("Fifth character must be a letter or a nunnber");
+            }
+            else { System.out.println("Incorrect registration");
+            }
+            //Rulling to make sure the registration is valid
             
             
             
-            String data2 = sc.nextLine();
-            System.out.println(data2);
+            
         }  
         } catch (Exception e) {
             System.out.println(e);
